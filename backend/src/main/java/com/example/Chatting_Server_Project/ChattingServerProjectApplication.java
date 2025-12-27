88f420a2bdd3fb7747e5705d33245317a97b0f1e
KMC\김명찬 <wtgs9817@naver.com>
2025-12-27 16:25:46 +0900

    프로젝트 생성

---
+++ backend/src/main/java/com/example/Chatting_Server_Project/ChattingServerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Chatting_Server_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingServerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingServerProjectApplication.class, args);
+	}
+
+}
