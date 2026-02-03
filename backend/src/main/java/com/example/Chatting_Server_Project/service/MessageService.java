2ce33018ea31c82828f9361c2d71fdb526a3361c
KMC\김명찬 <wtgs9817@naver.com>
2026-02-03 16:58:45 +0900

    완료

--- backend/src/main/java/com/example/Chatting_Server_Project/service/MessageService.java
+++ backend/src/main/java/com/example/Chatting_Server_Project/service/MessageService.java
@@ -3,11 +3,14 @@ package com.example.Chatting_Server_Project.service;
 
 import com.example.Chatting_Server_Project.DTO.MessageDTO;
 import com.example.Chatting_Server_Project.entity.MessageEntity;
+import com.example.Chatting_Server_Project.repository.MessageBatchRepository;
 import com.example.Chatting_Server_Project.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -18,14 +21,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Service
 public class MessageService {
 
-    private final MessageRepository messageRepository;
+    private final MessageBatchRepository messageBatchRepository;
     private final AtomicInteger count = new AtomicInteger(0);
     private final AtomicBoolean flushCheck = new AtomicBoolean(false);
     private final Queue<MessageEntity> messageBuffer = new ConcurrentLinkedQueue<>();
 
-
     public void addMessage(String RoomId, MessageDTO messageDTO) {
         messageDTO.setRoomId(RoomId);
+        if (messageDTO.getCreatedAt() == null) {
+            messageDTO.setCreatedAt(LocalDateTime.now());
+        }
         MessageEntity messageEntity = MessageEntity.toMessageEntity(messageDTO);
         messageBuffer.offer(messageEntity);
         int cnt = count.incrementAndGet();
@@ -35,32 +40,33 @@ public class MessageService {
         }
     }
 
-    @Scheduled(fixedRate = 300000)
+    @Scheduled(fixedRate = 50000)
     public void scheduledFlush() {
         if(count.get() > 0 && flushCheck.compareAndSet(false, true)) {
+
             flush();
         }
     }
 
     private void flush() {
-        try{
-            List<MessageEntity> list = new ArrayList<>();
-            MessageEntity entity;
+        try {
+            while (messageBuffer.peek() != null) {  // 버퍼 빌 때까지 반복
+                List<MessageEntity> list = new ArrayList<>();
+                MessageEntity entity;
 
-            while (list.size() <= 200 && (entity = messageBuffer.poll()) != null) {
-                list.add(entity);
+                while (list.size() < 200 && (entity = messageBuffer.poll()) != null) {
+                    list.add(entity);
+                }
+
+                if (!list.isEmpty()) {
+                    messageBatchRepository.batchInsert(list);
+                    count.addAndGet(-list.size());
+                }
+
             }
-
-            if(!list.isEmpty()) {
-                messageRepository.saveAll(list);
-                count.addAndGet(-list.size());
-            }
-        }
-        catch(Exception e) {
-            log.error("에러 : " , e);
-        }
-
-        finally{
+        } catch (Exception e) {
+            log.error("에러 : ", e);
+        } finally {
             flushCheck.set(false);
         }
     }
